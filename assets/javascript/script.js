c929860c55a23610c2bacaefeb5aaa5ae95721e5
Cristina Salvo <csalvo@spendsetter.com>
2017-07-25 13:02:46 -0400

    adding pictures & some styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -23,11 +23,14 @@ var punGame = {
     if (answer === punGame.puns[punGame.currentPunNumber]['correctAnswer']) {
       $("#pun").text("Correct!");
       punGame.correct++;
+
     } else if (answer !== punGame.puns[punGame.currentPunNumber]['correctAnswer']) {
       $("#pun").text("Wrong!");
       punGame.incorrect++;
     }
     clearInterval(timer);
+    $("#answer1").html("<img src='"+punGame.puns[punGame.currentPunNumber]['picture']+ "'>");
+    $("#answer2, #answer3").text("");  
     punGame.currentPunNumber++;
     setTimeout(punGame.nextPun, 3000);
   },
@@ -81,62 +84,62 @@ var punGame = {
     answer2: "It wasn't peeling well",
     answer3: "It kept getting bruises",
     correctAnswer: "answer2",
-    picture: "../images/banana.jpg"
+    picture: "assets/images/banana.jpg"
   }, {
     question: "What do you call a fancy fish?",
     answer1: "Dinner",
     answer2: "Sofishticated",
     answer3: "Fred",
     correctAnswer: "answer2",
-    picture: "../images/fish.jpg"
+    picture: "assets/images/fish.jpg"
   }, {
     question: "What do you call an alligator in a vest?",
     answer1: "An investigator",
     answer2: "Sherlock",
     answer3: "Fred",
     correctAnswer: "answer1",
-    picture: "../images/gator.jpg"
+    picture: "assets/images/gator.jpg"
   }, {
     question: "What do you call an everyday potato?",
     answer1: "Spuddy",
     answer2: "Too many potatoes",
     answer3: "A commontator",
     correctAnswer: "answer3",
-    picture: "../images/potato.jpg"
+    picture: "assets/images/potato.jpg"
   }, {
     question: "What do you call a labrador that does magic?",
     answer1: "A labracadabrador",
     answer2: "Highly unlikely",
     answer3: "Fred",
     correctAnswer: "answer1",
-    picture: "../images/labador.jpg"
+    picture: "assets/images/labrador.jpg"
   }, {
     question: "What do you call a bear with no teeth?",
     answer1: "Not scary",
     answer2: "Fred",
     answer3: "A gummy bear",
     correctAnswer: "answer3",
-    picture: "../images/gummybear.jpg"
+    picture: "assets/images/gummybear.jpg"
   }, {
     question: "What do you call a lazy kangaroo?",
     answer1: "A friend",
     answer2: "A pouch potato",
     answer3: "Fred",
     correctAnswer: "answer2",
-    picture: "../images/kangaroo.jpg"
+    picture: "assets/images/kangaroo.jpg"
   }, {
     question: "What do you get when dinosaurs crash their cars?",
     answer1: "A huge mess",
     answer2: "Unsafe driving conditions",
     answer3: "T-Rex",
     correctAnswer: "answer3",
-    picture: "../images/t-rex.jpg"
+    picture: "assets/images/t-rex.jpg"
   }, {
     question: "What do you call an owl that does magic?",
     answer1: "An owlcadabrador",
     answer2: "Hoodini",
     answer3: "Fred",
     correctAnswer: "answer2",
-    picture: "../images/owl.jpg"
+    picture: "assets/images/owl.jpg"
   }]
 };
